983171c0e5797f36e5b21834353cdf8eba8e527e
lishaocheng <gexbob@gmail.com>
2024-10-09 16:08:28 +0800

    update 00_template/00_Basic

--- 00_template/00_Basic/main.c
+++ 00_template/00_Basic/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 #include "Config.h"
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     printf("Project %s, Version %s\n", PROJECT, VERSION);
 
+    pr_info("info message\n");
+    pr_error("%d: %s", errno, strerror(errno));
     pr_debug("debug message\n");
-    
+
     return 0;
 }
